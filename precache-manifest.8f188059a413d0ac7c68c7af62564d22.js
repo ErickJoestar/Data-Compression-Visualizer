0272eaf1a8871fcaee0fa85ad36cf20e23b9bbc0
ErickJoestar <erickborquez@gmail.com>
2020-04-21 01:54:14 -0600

    Updates

---
+++ precache-manifest.8f188059a413d0ac7c68c7af62564d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4ffe04da7785a198351c425d5feaa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91754e18a8a90b6b4ee1",
+    "url": "/static/css/main.5336b637.chunk.css"
+  },
+  {
+    "revision": "8e733e0ec5240ad1529a",
+    "url": "/static/js/2.5ecd5775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ecd5775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91754e18a8a90b6b4ee1",
+    "url": "/static/js/main.fbc89524.chunk.js"
+  },
+  {
+    "revision": "7255f5b7cb0c2db56329",
+    "url": "/static/js/runtime-main.e118c219.js"
+  }
+]);
